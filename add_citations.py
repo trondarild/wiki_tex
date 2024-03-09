0f3de08e21deeebe98a68712a3ab893c958f51a8
trondarild <trondarild@gmail.com>
2024-03-09 21:35:59 +0100

    fix bugs

--- add_citations.py
+++ add_citations.py
@@ -10,7 +10,7 @@ def remove_duplicate_entries(bib_file_path):
         bib_content = f.read()
 
     # Regex to match bib entries. Assumes entries start with @ and are separated by at least one blank line
-    entry_pattern = re.compile(r'(@[a-zA-Z]+\{[^@]+?\}\n\n)', re.DOTALL)
+    entry_pattern = re.compile(r'(@[a-zA-Z]+\{[^@]+,?)', re.DOTALL)
     entries = entry_pattern.findall(bib_content)
 
     # Create a dictionary to hold entries, using the citation key as the unique identifier
@@ -26,12 +26,13 @@ def remove_duplicate_entries(bib_file_path):
     return unique_content
 
 def parse_bib_file(bib_data):
-    
+    print(bib_data[0:10])
     parser = bibtex.Parser()
     bib_structure = {}
     try:
         bib_data = parser.parse_string(bib_data)
         # bib_data = parse_bib_file(bib_file_path, check_duplicates=False)
+        # print(bib_data)
     except BibliographyDataError as e:
         print(f"An error occurred while reading the BibTeX file: {e}")
         return bib_structure
@@ -54,18 +55,23 @@ def convert_citations(text, bib_structure):
     def citation_replacement(match):
         prefix = "(" if match.group(1) else ""
         cite_key = match.group(2)
+        
         if cite_key in bib_structure:
             entry = bib_structure[cite_key]
             authors = entry['authors'].split(' and ')[0].split(',')[0]  # Assuming first author only
             year = entry['year']
-            citation = f"{authors} et al., ({year})"
+            citation = f"{authors} et al., "
+            citation+= f"{year}" if match.group(1) else f"({year})"
             used_citations.append(cite_key)
             return f"{prefix}{citation}{')' if prefix else ''}"
+        # else:
+        #     print('not found citekey: ' + cite_key)
         return match.group(0)
 
     text = cite_regex.sub(citation_replacement, text)
 
     # Generate APA style citation list
+    # TODO sort list
     citation_list = "==References==\n"
     for cite_key in set(used_citations):  # Remove duplicates in used citations
         entry = bib_structure[cite_key]
@@ -86,10 +92,12 @@ if __name__ == '__main__':
     latex_content = sys.stdin.read()
     bib_file_path = sys.argv[1]
     bib_data = remove_duplicate_entries(bib_file_path)
+    print('after remove duplicates')
+    print(bib_data[0:10])
     with open('refs_no_duplicates.bib', 'w') as f:
         f.write(bib_data)
     bib_structure = parse_bib_file(bib_data)
     converted_text = convert_citations(latex_content, bib_structure)
-    #with open(text_file_path.replace('.txt', '_with_citations.txt'), 'w') as f:
-    #    f.write(converted_text)
-    print(converted_text)
+    with open('out_with_citations.txt', 'w') as f:
+        f.write(converted_text)
+    #print(converted_text)
