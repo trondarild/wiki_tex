346fe7ab52f318fcb9e76e82a52f3cec9e2a63c8
trondarild <trondarild@gmail.com>
2024-03-09 16:12:48 +0100

    fix bugs

--- wiki_tex.py
+++ wiki_tex.py
@@ -21,12 +21,14 @@ converter.  With that in mind, I hope it's useful.
 """
 
 import sys
+import re
 
 def get_latex_file(base_web_filename):
-  latex_file = open(base_web_filename+".tex","r")
-  source = latex_file.read()
-  latex_file.close()
-  return source
+    with open(base_web_filename+".tex", "r") as latex_file:
+        lines = latex_file.readlines()
+    # Remove leading whitespace from each line
+    cleaned_lines = [line.lstrip() for line in lines]
+    return '\n'.join(cleaned_lines)
 
 def remove_comments(source):
   for comments in range(source.count('%')):
@@ -34,6 +36,21 @@ def remove_comments(source):
     end = source.find("\n",start)
     source = source[:start]+source[end:]
   return source
+
+def remove_index_entries(source):
+    index_command = "\\index{"
+    start_index = 0
+    while True:
+        start_loc = source.find(index_command, start_index)
+        if start_loc == -1:  # No more \index{} commands to process
+            break
+        end_loc = find_closing_bracket(source, start_loc + len(index_command) - 1)
+        # Remove the \index{...} including the closing bracket
+        source = source[:start_loc] + source[end_loc + 1:]
+        start_index = start_loc  # Update start_index to current position for next search
+    return source
+
+
 
 def replace_labelled_item(source):
   for j in range(source.count("\item[")):
@@ -89,21 +106,32 @@ def simple_replacements(source):
   return source
 
 def bracketed_replacements(source):
-  replacements = {"\\emph{":"\'\'",
-                  "\\textbf{":"\'\'\'",
-                  "\\chapter{":"==",
-                  "\\chapter*{":"==",
-                  "\\section{":"===",
-                  "\\section*{":"===",
-                  "\\subsection{":"====",
-                  "\\subsection*{":"===="}
+  replacements = {
+                  "\\emph{":"\'\'", # Existing entry for emphasis (italics)
+                  "\\textbf{":"\'\'\'", # Adding support for bold
+                  "\\textit{":"\'\'", # Adding support for italics
+                  "\\chapter{":"\n==",
+                  "\\chapter*{":"\n==",
+                  "\\section{":"\n===",
+                  "\\section*{":"\n===",
+                  "\\subsection{":"\n====",
+                  "\\subsection*{":"\n===="
+                 }
+  
+  
   for string in replacements:
     for j in range(source.count(string)):
       current_location = source.find(string)
-      source = source.replace(string,replacements[string],1)
-      right_bracket = find_closing_bracket(source,current_location)
-      source = source[:right_bracket]+replacements[string]+source[right_bracket+1:]
+      closing_markup = replacements[string] # Save closing markup for later
+      source = source.replace(string, replacements[string], 1) # Open markup replacement
+      right_bracket = find_closing_bracket(source, current_location)
+      # Use closing_markup for symmetric markup (bold/italics), handle special cases as needed
+      newline = '\n\n' if closing_markup[1] == '=' else ''
+      source = source[:right_bracket] + closing_markup + newline + source[right_bracket+1:] # Close markup replacement
+  
+  
   return source
+
 
 def replace_dollar(source):
   while source.find("$$") != -1:
@@ -128,6 +156,15 @@ def replace_link(source):
     source = source[:current_location]+"["+url+" "+anchor+"]"+source[second_right_bracket+1:]
   return source
 
+def regex_replacements(source):
+    replacements = {
+        r'\\clearpage.+': '',  # Remove \index{} entries
+        # Add more regex patterns and their replacements as needed
+    }
+    for pattern, replacement in replacements.items():
+        source = re.sub(pattern, replacement, source)
+    return source
+
 def strip_lines(string):
   location = 0
   while string.find("\n",location) != -1:
@@ -140,32 +177,46 @@ def strip_lines(string):
 
 def extract_article(source,article_number,num_articles,base_web_filename):
   source = remove_comments(source)
+  source = remove_index_entries(source)
   source = simple_replacements(source)
   source = replace_labelled_item(source)
   source = bracketed_replacements(source)
   source = replace_dollar(source)
   source = replace_link(source)
+  source = regex_replacements(source)
   source = source.strip("\n")
   source = source.replace("\n\n\n","\n\n")
   source = strip_lines(source)
-  print ("Creating article number %s." % article_number)
-  article_file = open(base_web_filename+str(article_number)+".wiki",'w')
-  print ("Creating article number %s." % article_number)
+
+  return source
+  # print ("Creating article number %s." % article_number)
+  
+
+def write_to_file(base_web_filename, article_number):
+  # article_file = open(base_web_filename+str(article_number)+".wiki",'w')
+  # print ("Creating article number %s." % article_number)
   article_file = open(base_web_filename+str(article_number)+".wiki",'w')
   article_file.write(source)
   article_file.close()
 
 base_web_filename = sys.argv[1]
 source = get_latex_file(base_web_filename)
+
 source = source.replace("\\end{document}","")
+
 num_articles = source.count('%#break')
-print (num_articles)
+# print (num_articles)
 if num_articles == 0:
   num_articles = 1
   source = source.replace("\\begin{document}",'%#break',1)
 
 for article_number in range(num_articles):
-  source = source[(source.find('%#break')+8):]
-  if (article_number+1) < num_articles: article_source = source[0:source.find('%#break')]
-  else: article_source = source
-  extract_article(article_source,article_number,num_articles,base_web_filename)
+  # source = source[(source.find('%#break')+8):]
+  if (article_number+1) < num_articles: 
+    article_source = source[0:source.find('%#break')]
+  else: 
+    article_source = source
+  # print(article_source[0:10])
+  source = extract_article(article_source,article_number,num_articles,base_web_filename)
+  print(source)
+
